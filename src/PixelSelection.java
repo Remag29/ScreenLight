cca81c639a7cb52d89051c3ccdc3781f8e16d02b
Remag29 <remag29@outlook.com>
2022-05-21 22:04:35 +0200

    Adding PixelSelection class

---
+++ src/PixelSelection.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+import java.util.LinkedList;
+
+public class PixelSelection {
+
+    private final LinkedList<Pixel> pixellist;
+
+    public PixelSelection() {
+        this.pixellist = new LinkedList<>();
+    }
+
+    public Pixel getPixel(int index) {
+        return this.pixellist.get(index);
+    }
+
+    public void addPixel(int x, int y, Color color) {
+        this.pixellist.add(new Pixel(x, y, color));
+    }
+
+    public Color getColorAverage() {
+        int r = 0, g = 0, b = 0;
+        for (Pixel pixel :
+                this.pixellist) {
+            r += pixel.getColor().getRed();
+            g += pixel.getColor().getGreen();
+            b += pixel.getColor().getBlue();
+        }
+        r /= pixellist.size();
+        g /= pixellist.size();
+        b /= pixellist.size();
+
+        return new Color(r, g, b);
+    }
+
+    public int size() {
+        return this.pixellist.size();
+    }
+
+    @Override
+    public String toString() {
+        //TODO
+        return null;
+    }
+}
